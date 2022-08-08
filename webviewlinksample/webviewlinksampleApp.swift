9a1c8bb3551d639c7c9ac13caee3ad1ac281518f
lethe2211 <lethe2211@gmail.com>
2022-08-08 22:46:19 +0900

    Initial Commit

---
+++ webviewlinksample/webviewlinksampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewlinksampleApp.swift
+//  webviewlinksample
+//
+//  Created by Shogen, Shuhei | Lethe | ELPD on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct webviewlinksampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
